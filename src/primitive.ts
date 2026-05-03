0e18f2177b9a4b81f22bf479e40750b85d4110ae
CreativeGhalib <creativeghalib@gmail.com>
2026-05-04 00:10:04 +0600

    Add primitive type examples

---
+++ src/primitive.ts
@@ -0,0 +1,10 @@
+// JS-TS
+//JS:string ,number boolean ,null ,undefined ,symbol
+// TS:never,unknown,any,void
+let userName:string="mesbah123" //explacitly declare the type of variable
+let isAdmin=true  //implicitly declare the type of variable
+let age:number=30
+let course: string = "next level web development"
+// example of tuple and mixedArr
+let mixedArr: (string|number|boolean)[] = [1, "mesbah", true, 2, "typescript", false]
+let tuple: [string, number, boolean] = ["mesbah", 30, true]
